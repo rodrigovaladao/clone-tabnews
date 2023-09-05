c69a797fe9e0cbf3c6d2585d16dced77f58ae9dc
Rodrigo Valadao <69178087+rodrigovaladao@users.noreply.github.com>
2023-09-05 07:55:47 +0000

    Adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementation of https://www.tabnews.com.br for the course https://curso.dev @filipedeschamps
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-  return <h1>Te amo minha Koalinha! </h1>
-
+function Home() {
+  return <h1>Te amo minha Koalinha! </h1>;
 }
 
 export default Home;
